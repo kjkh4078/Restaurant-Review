cb697ad2adeceed7e94a6a490569e1725f2caaef
kjkh4078 <93776487+kjkh4078@users.noreply.github.com>
2023-10-30 22:43:42 +0900

    Delete src/components directory

--- src/components/button.js
+++
@@ -1,6 +0,0 @@
-import classNames from "classnames";
-import styles from "./Button.module.scss";
-const Button = ({ text, onClick, className, }) => {
-    return (React.createElement("button", { onClick: onClick, className: classNames(styles.btn, className === null || className === void 0 ? void 0 : className.map((item) => styles[item])) }, text));
-};
-export default Button;
