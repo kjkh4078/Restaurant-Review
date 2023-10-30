a8b10eebc060006c1bfb48c90dad89285a96fdf4
kjkh4078 <93776487+kjkh4078@users.noreply.github.com>
2023-10-31 01:30:05 +0900

    Add files via upload

---
+++ fbase.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDS6wJYPJuOQLPS-gfh6toOq_taP0cbvkM",
+  authDomain: "crawling-d59b6.firebaseapp.com",
+  projectId: "crawling-d59b6",
+  storageBucket: "crawling-d59b6.appspot.com",
+  messagingSenderId: "546537284543",
+  appId: "1:546537284543:web:f28e4f3fd27ec1b4efb70a",
+};
+
+export const fireBase = initializeApp(firebaseConfig);
+export const authService = getAuth();
+export const storageService = getStorage();
+export const dbService = getFirestore();
