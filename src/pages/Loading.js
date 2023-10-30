d3abf9f838bc10ad3df05130a000eeee265f707e
kjkh4078 <93776487+kjkh4078@users.noreply.github.com>
2023-10-30 22:43:48 +0900

    Delete src/pages directory

--- src/pages/Loading.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import styles from "./Loading.module.scss";
-const Loading = () => {
-    return (React.createElement("div", { className: styles.container },
-        React.createElement("div", { className: styles.loading }, "\u25CC")));
-};
-export default Loading;
